d2cd8466669818bea3ea8412e9cc98058f06b7da
RenyV19 <115494642+RenyV19@users.noreply.github.com>
2023-06-21 23:33:40 -0600

    correcion de error

--- Ejercicio 1 epiko/Program.cs
+++ Ejercicio 1 epiko/Program.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace Ejercicio_1_epiko
 {
-    internal class Program
+    public class Program
     {
         // * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100
         // (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
         // * - Múltiplos de 3 por la palabra "fizz".
         // * - Múltiplos de 5 por la palabra "buzz".
         // * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int num = 100;
 
